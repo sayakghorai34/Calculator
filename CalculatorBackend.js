e42317573a77ca35bb48ce6392097f3d38d35412
Sayak Ghorai <sayakghorai@Sayaks-MacBook-Air-M1.local>
2024-01-23 17:37:06 +0530

    final2

--- CalculatorBackend.js
+++ CalculatorBackend.js
@@ -6,25 +6,45 @@ var res = 0;
 var buffer = 0;
 var flag = 0;
 var op = "";
-function getValue(num){
-    if(flag == 0){
+
+function getValue(num) {
+    if (flag == 0) {
         num1 += num;
         n1 = parseFloat(num1);
-    }
-    else{
+        document.getElementById("display").value = n1;
+    } else {
         num2 += num;
         n2 = parseFloat(num2);
+        document.getElementById("display").value = n2;
     }
-    
 }
 
-function getFunc(sym){
+function getFunc(sym) {
+    if (flag == 1) {
+        evaluate();
+        num1 = buffer.toString();
+        num2 = "";
+        n1 = buffer;
+        n2 = 0;
+        flag = 0;
+    }
+    
     flag = 1;
     op = sym;
 }
 
-function getRes(){
-    switch(op){
+function clearDisplay() {
+    num1 = "";
+    num2 = "";
+    n1 = 0;
+    n2 = 0;
+    flag = 0;
+    op = "";
+    document.getElementById("display").value = "";
+}
+
+function evaluate() {
+    switch(op) {
         case "+":
             res = n1 + n2;
             break;
@@ -38,12 +58,20 @@ function getRes(){
             res = n1 / n2;
             break;
     }
+    
+    document.getElementById("display").value = res;
     buffer = res;
-    num1 = "";
+    num1 = res.toString();
     num2 = "";
-    n1 = 0;
+    n1 = res;
     n2 = 0;
-    flag = 0;
     op = "";
-    return res;
+}
+
+// Function for handling the equals button
+function equals() {
+    if (flag == 1) {
+        evaluate();
+        flag = 0;
+    }
 }
